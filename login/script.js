38836bc29b7426c50dd0b65eda4019fe3a052b7d
Rodolfo Lepe <rudylepe5@gmail.com>
2018-02-13 18:09:26 -0600

    Creacion de cuenta como restaurante + agregar mesas

--- login/script.js
+++ login/script.js
@@ -17,6 +17,8 @@ $(document).ready(function(){
     var dbRef = firebase.database();
     var contactsRef = dbRef.ref('contacts')
     var usersRef = dbRef.ref('users')
+    var restaurantRef = dbRef.ref('restaurants')
+    var tableRef = dbRef.ref('tables')
     var auth = null;
     var provider = new firebase.auth.FacebookAuthProvider();
 
@@ -64,6 +66,63 @@ $(document).ready(function(){
               auth = user;
               //now saving the profile data
               usersRef
+                .child(user.uid)
+                .set(data)
+                .then(function(){
+                  console.log("User Information Saved:", user.uid);
+                })
+              $('#messageModalLabel').html(spanText('Success!', ['center', 'success']))
+              //hide the modal automatically
+              setTimeout(function() {
+                $('#messageModal').modal('hide');
+                $('.unauthenticated, .userAuth').toggleClass('unauthenticated').toggleClass('authenticated');
+                contactsRef.child(auth.uid)
+                  .on("child_added", function(snap) {
+                    console.log("added", snap.key(), snap.val());
+                    $('#contacts').append(contactHtmlFromObject(snap.val()));
+                  });
+              }, 500);
+              console.log("Successfully created user account with uid:", user.uid);
+              $('#messageModalLabel').html(spanText('Successfully created user account!', ['success']))
+            })
+            .catch(function(error){
+              console.log("Error creating user:", error);
+              $('#messageModalLabel').html(spanText('ERROR: '+error.code, ['danger']))
+            });
+        } else {
+          //password and confirm password didn't match
+          $('#messageModalLabel').html(spanText("ERROR: Passwords didn't match", ['danger']))
+        }
+      }  
+    });
+
+    //Register of restaurant
+    $('#doRegisterRestaurant').on('click', function (e) {
+      e.preventDefault();
+      $('#registerRestaurant').modal('hide');
+      $('#messageModalLabel').html(spanText('<i class="fa fa-cog fa-spin"></i>', ['center', 'info']));
+      $('#messageModal').modal('show');
+      var data = {
+        email: $('#registerEmail').val(), //get the email from Form
+        name: $('#registerName').val(), // get firstName
+        address: $('#registerAddress').val(), // get lastName
+      };
+      var passwords = {
+        password : $('#registerPassword').val(), //get the pass from Form
+        cPassword : $('#registerConfirmPassword').val(), //get the confirmPass from Form
+      }
+      if( data.email != '' && passwords.password != ''  && passwords.cPassword != '' ){
+        if( passwords.password == passwords.cPassword ){
+          //create the user
+          
+          firebase.auth()
+            .createUserWithEmailAndPassword(data.email, passwords.password)
+            .then(function(user){
+              //now user is needed to be logged in to save data
+              console.log("Authenticated successfully with payload:", user);
+              auth = user;
+              //now saving the profile data
+              restaurantRef
                 .child(user.uid)
                 .set(data)
                 .then(function(){
@@ -152,7 +211,28 @@ $(document).ready(function(){
         //inform user to login
       }
     });
+
+   //add table
+    $('.addTable').on("click", function( event ) {  
+      event.preventDefault();
+        if( auth != null ){
+          if( $('#table').val() != '' || $('#numberSeats').val() != '' ){
+            tableRef.child(auth.uid)
+              .push({
+                table: $('#table').val(),
+                numberSeats: $('#numberSeats').val(),
+              })
+              document.contactForm.reset();
+          } else {
+            alert('Please fill at-lease name or email!');
+          }
+        } else {
+          //inform user to login
+        }
+      });
   })
+
+
    
   //prepare contact object's HTML
   function contactHtmlFromObject(contact){
